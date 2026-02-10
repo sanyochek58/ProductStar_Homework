eeb3553989e8e90572d050c8a0df2763c7ce4c89
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-02-10 09:36:31 +0300

    Созданы 2 базовых класса

---
+++ HomeWork_OOP/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Max", 20, "MIREA");
+        Docent docent = new Docent("Andrew", 26, "MIREA");
+
+        System.out.println(student);
+        System.out.println(docent);
+    }
+}
