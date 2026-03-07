b45df65188f5df860b071317483ccae091a31343
Alexandr Buraev <148450042+sanyochek58@users.noreply.github.com>
2026-03-07 12:57:37 +0300

    Добавлены тесты в реализацию задачи

--- CircleWork/src/Main.java
+++ CircleWork/src/Main.java
@@ -2,5 +2,12 @@ public class Main{
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         System.out.println(circle.getArea());
+
+//        circle.setRadius(-1);
+//        System.out.println(circle.getArea());
+
+        TestCircle.test();
+
+
     }
 }
